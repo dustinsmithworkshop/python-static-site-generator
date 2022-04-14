07946d840c2dbfd63fa364965d42617b4fe83032
Dustin Smith <dsmithworkshop@yahoo.com>
2022-04-14 17:48:59 -0500

    fixes

--- ssg/content.py
+++ ssg/content.py
@@ -1,6 +1,7 @@
 import re
-from yaml import load, FullLoader
+
 from collections.abc import Mapping
+from yaml import load, FullLoader
 
 class Content(Mapping):
 	__delimiter = r"^(?:-|\+){3}\s*$"
@@ -8,38 +9,38 @@ class Content(Mapping):
 
 	@classmethod
 	def load(cls, string):
-		 _, fm, content = cls.__regex.split(string, 2)
-		 metadata = load(fm, Loader=FullLoader)
-		 return cls(metadata, content)
+		_, fm, content = cls.__regex.split(string, 2)
+		metadata = load(fm, Loader=FullLoader)
+		return cls(metadata, content)
 
-    def __init__(self, metadata, content):
-        self.data = metadata
-        self.data["content"] = content
+	def __init__(self, metadata, content):
+		self.data = metadata
+		self.data["content"] = content
 
-    @property
-    def body(self):
-        return self.data["content"]
+	@property
+	def body(self):
+		return self.data["content"]
 
-    @property
-    def type(self):
-        return self.data["type"] if "type" in self.data else None
+	@property
+	def type(self):
+		return self.data["type"] if "type" in self.data else None
 
-    @type.setter
-    def type(self, type):
-        self.data["type"] = type
+	@type.setter
+	def type(self, type):
+		self.data["type"] = type
 
-    def __getitem__(self, key):
-        return self.data[key]
+	def __getitem__(self, key):
+		return self.data[key]
 
-    def __iter__(self):
-        self.data.__iter__()
+	def __iter__(self):
+		self.data.__iter__()
 
-    def __len__(self):
-        return len(self.data)
+	def __len__(self):
+		return len(self.data)
 
-    def __repr__(self):
-        data = {}
-        for key, value in self.data.items():
-            if key != "content":
-                data[key] = value
-        return str(data)
+	def __repr__(self):
+		data = {}
+		for key, value in self.data.items():
+			if key != "content":
+				data[key] = value
+		return str(data)
